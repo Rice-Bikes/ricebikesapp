81b7703cd180d39236bf7bc87e67b5d8ff99bf64
melanie-09 <143124193+melanie-09@users.noreply.github.com>
2024-12-31 01:35:12 -0500

    transactions display

---
+++ src/components/TransactionPage/TransactionPage.tsx
@@ -0,0 +1,11 @@
+import { useState, FormEvent } from "react"; // React State Hook
+import ReactDOM from 'react-dom/client';
+
+
+
+
+function 
+
+const Transaction = ;
+
+export default Transaction
---
+++ src/components/TransactionPage/TypeDropdown.tsx
@@ -0,0 +1,46 @@
+import {useState} from 'react';
+
+function TypeDropdown( props ) {
+    const [display, setDisplay] = useState('none')
+    
+    function handleClick() {
+        if (display == 'none') {
+            setDisplay('block')
+        } else {
+            setDisplay('none')
+        }
+    }
+    return (
+        <div>
+            <div>
+            Hello World 
+            </div>
+            <div style={{display:display}}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export { Dropdown }
+
+function DropdownElement(props) {
+    var content = props.name
+    return (
+        <div>
+            {content}
+        </div>
+    )
+}
+
+function DropdownButton (props) {
+    var content = props.content;
+    var handleClick = props.onClick;
+    return (
+        <div>
+            <div>
+                { content }
+            </div>
+        </div>
+    )
+}
